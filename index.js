6eddf606f9d7d7d88a8862a274ead20680556c57
eth4kr <eth4kr@gmail.com>
2017-12-24 15:05:09 +0530

    v1.o completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var arg = process.argv[2];
+log=function(s){console.log(s)}
 var xor = function(a,b) {
   return ( a || b ) && !( a && b );
 }
@@ -18,12 +20,11 @@ var haxh = function(str) {
 	}
 	this.bin_arr = [];
 	count=-1;
-	for(i=0; i<=512;){
+	for(i=0; i<512;){
 		count++
 		this.bin_arr[count] = this.binary.slice(i, i+16);
 		i+=32;
 	}
-	console.log(this.binary.length)
 	last_arr = this.bin_arr[count].toString()
 	count = last_arr.length-1
 	for(i=this.bin_len.length-1; i>=0; i--) {
@@ -33,11 +34,34 @@ var haxh = function(str) {
 	}
 	this.bin_arr[this.bin_arr.length-1] = last_arr
 
-	for(i=0; i<=16;i++) {
-
+	var iMax = 16;
+	var jMax = 4;
+	this.data = new Array();
+	for (i=0; i<iMax; i++) {
+	 	this.data[i] = new Array();
+ 			for (j=0; j<jMax; j++) {
+		  	this.data[i][j] = 0;
+	 	}
 	}
-
+	c=0
+	for(k=0;k<16;k++) {
+		c=0
+		for(i=0; i<16; i+=4) {
+			this.data[k][c] = parseInt(this.bin_arr[k].slice(i, i+4)).toString(16);
+			c++;
+		}
+	}
+	c=0
+	this.e=''
+	for(k=0;k<16;k++) {
+		c=0
+		for(i=0; i<16; i+=4) {
+			this.e+= (this.data[k][c]=='0')?'':this.data[k][c];
+			c++;
+		}
+	}
+	
 }
 
-var t = new haxh('CRYPTO');
-// console.log(t.bin_arr);
+var t = new haxh(arg);
+log(t.e)
